b18b4d6d929cae7b6e06d0d9ce0b1bf0ac662882
JohnAsare <johnasare7@gmail.com>
2020-05-06 22:49:38 -0400

    Wrote the check_push function

--- hello_world.py
+++ hello_world.py
@@ -3,5 +3,17 @@
 
 """ Writing the first code for the Google Python Certificate """
 
-print("Hello World!")
+
+def check_push(name):
+
+    if name.isalpha:
+        print("Howdy World!, I will call you by {}" .format(name))
+    else:
+        print("Please type a name")
+
+
+check_push(name=input("What is your name? "))
+
+
+
 
